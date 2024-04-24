a591f652c3b435b849e34f6e819aa8a84c9d3bf7
Heeneth12 <heeneth123@gmail.com>
2024-04-24 11:14:13 +0530

    added swagger

--- src/main/java/com/backend/Retrospect/user/controller/UserController.java
+++ src/main/java/com/backend/Retrospect/user/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
