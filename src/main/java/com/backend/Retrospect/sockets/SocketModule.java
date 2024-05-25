706f8b7ce53b472481b997ecbf73a1a6c59906aa
Heeneth12 <heeneth123@gmail.com>
2024-05-25 13:09:47 +0530

    added now socket events for user_join and user_exit room

--- src/main/java/com/backend/Retrospect/sockets/SocketModule.java
+++ src/main/java/com/backend/Retrospect/sockets/SocketModule.java
@@ -1,11 +1,9 @@
 package com.backend.Retrospect.sockets;
+
 import com.backend.Retrospect.room.repository.IRoomRepository;
-import com.backend.Retrospect.roomToUser.dto.UserRoomJoinDTO;
 import com.backend.Retrospect.roomToUser.repository.IRoomToUserRepository;
 import com.backend.Retrospect.roomToUser.service.RoomToUserService;
-import com.backend.Retrospect.sockets.constants.Constants;
 import com.backend.Retrospect.sockets.entiry.Message;
-import com.backend.Retrospect.user.entity.UserEntity;
 import com.backend.Retrospect.user.repository.IUserRepository;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
@@ -14,7 +12,6 @@ import com.corundumstudio.socketio.listener.DisconnectListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 
 @Component
 @Slf4j
@@ -34,26 +31,22 @@ public class SocketModule {
     @Autowired
     private RoomToUserService roomToUserService;
 
-
     public SocketModule(SocketIOServer server, SocketService socketService) {
         this.server = server;
         this.socketService = socketService;
         server.addConnectListener(onConnected());
         server.addDisconnectListener(onDisconnected());
         server.addEventListener("send_message", Message.class, onChatReceived());
-
     }
-
 
     private DataListener<Message> onChatReceived() {
         return (senderClient, data, ackSender) -> {
-            log.info("Received message from client {}: {} : {}", senderClient.getSessionId(), data.getContent() ,data.getContentType());
+            log.info("Received message from client {}: {} : {}", senderClient.getSessionId(), data.getContent(), data.getContentType());
             socketService.saveMessage(senderClient, data);
             // Handle sending the message to other clients
             server.getRoomOperations(data.getRoom()).sendEvent("receive_message", data);
         };
     }
-
 
     private ConnectListener onConnected() {
         return (client) -> {
@@ -61,25 +54,24 @@ public class SocketModule {
             String room = String.join("", params.get("room"));
             String username = String.join("", params.get("username"));
 
-            String contentType = "connected";
             client.joinRoom(room);
-//            socketService.saveInfoMessage(client, String.format(Constants.WELCOME_MESSAGE, username), room ,username ,contentType);
-            log.info("Socket ID[{}] - room[{}] - username[{}] - content[{}] connected to chat", client.getSessionId().toString(), room, username, contentType);
+
+            // Emit a message to all clients in the room about the new user
+            server.getRoomOperations(room).sendEvent("user_join" ,username);
+
+            log.info("Socket ID[{}] - room[{}] - username[{}] connected to chat", client.getSessionId().toString(), room, username);
         };
     }
-
-
-
 
     private DisconnectListener onDisconnected() {
         return client -> {
             var params = client.getHandshakeData().getUrlParams();
             String room = String.join("", params.get("room"));
             String username = String.join("", params.get("username"));
-            String contentType = "disconnected";
-//            socketService.saveInfoMessage(client, String.format(Constants.DISCONNECT_MESSAGE, username), room ,username , contentType);
-            log.info("Socket ID[{}] - room[{}] - username [{}]  disconnected to chat module through", client.getSessionId().toString(), room, username);
+
+            server.getRoomOperations(room).sendEvent("user_exit" ,username);
+
+            log.info("Socket ID[{}] - room[{}] - username [{}] disconnected from chat", client.getSessionId().toString(), room, username);
         };
     }
-
 }
